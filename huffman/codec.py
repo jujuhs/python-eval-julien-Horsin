df2add616ea582c7df109b068ccb4ba35a676050
Julien Horsin <julien.horsin@mines-paristech.fr>
2020-03-28 10:35:53 +0100

    commentaire huffman

--- huffman/codec.py
+++ huffman/codec.py
@@ -1,8 +1,8 @@
-class Node:
+class Node: #classe des feuilles
     def __init__(self,valeur, fg, fd=None):
-        self.valeur = valeur
-        if fd == None:
-            self.name = fg
+        self.valeur = valeur #le poids du noeud
+        if fd == None:  #dans ce cas, il n'y a pas d'enfant (c'est une feuille)
+            self.name = fg #l'attribut name regroupe les noms des enfants
             self.fg = None
             self.fd = None
 
@@ -14,7 +14,7 @@ class Node:
         return f'{self.valeur},{self.name}'
 
 
-class TreeBuilder:
+class TreeBuilder(Node):
     def __init__(self, text):
         self.text = text
         self.lst = list(self.text)
@@ -31,63 +31,70 @@ class TreeBuilder:
         return letters
 
 
-    def arbre(self):
-        Letters = self.occur()
+    def tree(self):
+        Letters = self.occur() #liste contenant à chaque fois un doublet (lettre, nombre d'apparition)
+        #et classé par ordre croissant
         Tot_feuilles = []
         for i in Letters:
-            Tot_feuilles.append(Node(i[1],i[0]))
-        while len(Tot_feuilles) > 1:
-            f1 = Tot_feuilles[0]
+            Tot_feuilles.append(Node(i[1],i[0])) #on crée les feuilles (tout en bas de l'arbre)s
+
+        while len(Tot_feuilles) > 1: #on va regrouper les feuilles en noeud, jusqu'à avoir un seul noeud
+            f1 = Tot_feuilles[0] #on prend les deux noeuds avec le moins d'apparition
             f2 = Tot_feuilles[1]
-            v =f1.valeur + f2.valeur
-            
-            new_feuilles = Node(v, f1, f2)
-            del Tot_feuilles[1]
+            v =f1.valeur + f2.valeur #le poids du noeud composé des deux noeuds
+
+            new_noeud = Node(v, f1, f2) #on crée notre nouveau noeud
+
+            del Tot_feuilles[1]  #on retire les deux anciens noeuds de la liste des noeuds à traiter
             del Tot_feuilles[0]
             j=0
-            while j<len(Tot_feuilles) and Tot_feuilles[j].valeur <= new_feuilles.valeur:
+            #puis on va placer ce nouveau noeud pour en fonction de son poids
+            while j<len(Tot_feuilles) and Tot_feuilles[j].valeur <= new_noeud.valeur:
                     j+=1
             if j==0:
-                Tot_feuilles.append(new_feuilles)
+                Tot_feuilles.append(new_noeud)
             else:
-                Tot_feuilles.insert(j, new_feuilles)
-        return Tot_feuilles[0]
-
-
+                Tot_feuilles.insert(j, new_noeud)
+        return Tot_feuilles[0]  #on renvoie le noeud principal
+        #à partir du ce noeud (appelé racine), on peut accéder à tout l'arbre
 
 
    
 
-class Codec:
-
- 
+class Codec: 
     def __init__(self, racine):
-        self.racine = racine
+        self.racine = racine #le noeud le plus haut
         self.dic={}
+        self.create()
 
         
-    def create(self, noeud=None,code=""):
-        if noeud==None:
+    def create(self, noeud=None,code=""): #on créer le dictionnaire de codage (lettre:code avec huffman)
+        # de manière recursive
+
+        if noeud==None: #on commence en haut de l'arbre
             noeud = self.racine
 
-        if len(noeud.name)==1:
+        if len(noeud.name)==1: #cas de base
             self.dic[noeud.name]=code
         else:
-            code1 = code +"0"
-            code2 = code + "1"
-            return self.create(noeud.fg,code1),self.create(noeud.fd,code2)
+            code1 = code +"0" #0 si on va vers le fils de gauche
+            code2 = code + "1" #1 si on va vers le fils de droite
+            return self.create(noeud.fg,code1),self.create(noeud.fd,code2) #recursivité jusqu'à atteindre une feuille
 
     def encode(self,text):
         codage =str()
         for i in text:
-            codage = codage + self.dic[i]
+            codage = codage + self.dic[i] #on code chaque lettre
         return codage
+
     def decode(self,code):
+        # pour décoder, on part de la racine et on descends, jusqu'à tomber sur une feuille
         decodage =str()
         i=0
         while i<len(code):
-            noeud = self.racine
+            noeud = self.racine #on repart de la racine
             while len(noeud.name) > 1:
+                #on regarde si on doit aller à gauche ou à droite
                 if code[i]=="0":
                     noeud = noeud.fg
                 else:
@@ -95,26 +102,3 @@ class Codec:
                 i+=1
             decodage = decodage + noeud.name
         return decodage
-            
-
-            
-        
-
-
-
-
-text = "a dead dad ceded a bad babe a beaded abaca bed"
-sol="1000011101001000110010011101100111001001000111110010011111011111100010001111110100111001001011111011101000111111001"
-
-# on analyse les fréquences d'occurrence dans text
-# pour fabriquer un arbre binaire
-builder = TreeBuilder(text)
-a= builder.arbre()
-#print(a.fg.name)
-#print(a.fd)
-codec = Codec(a)
-codec.create()
-#print(codec.dic)
-encoded = codec.encode(text)
-print(encoded)
-print(codec.decode(encoded))
